d5c0a4c0df01483c7065333244075ff932f58e1d
vovanluan <vovanluan201294@gmail.com>
2015-10-13 23:32:43 +0700

    Add jump and remove trash tile

--- Gunny.py
+++ Gunny.py
@@ -172,11 +172,12 @@ def main(screen):
                 if event.key == Constant.PLAYER1FIREKEY:
                     player.fire_down = True
                 elif event.key == Constant.PLAYER1CHANGEBULLET:
-
                     player.typeOfBullet += 1
                     if player.typeOfBullet >= Constant.NUM_BULLET_TYPE:
                         player.typeOfBullet = EBulletType.BASIC
-
+                elif event.key == Constant.PLAYER1JUMPKEY:
+                    if not player.downable:
+                        player.jump = 15
             elif event.type == KEYUP:
                 if event.key == Constant.PLAYER1FIREKEY:
                     player.fire_down = False
@@ -242,11 +243,10 @@ def main(screen):
                 if player.direction == 1:
                     if player.pos[0] + Constant.PLAYERWIDTH >= tile.pos[0] \
                             and player.pos[0] + Constant.PLAYERWIDTH  <= tile.pos[0] + Constant.TILE_WIDTH:
-                        #print player.pos, tile.pos, tile.id
+                        print player.pos, tile.id
                         player.isBlockByWall = True
                 else:
                     if player.pos[0]  >= tile.pos[0] and player.pos[0] <= tile.pos[0] + Constant.TILE_WIDTH:
-                        print player.pos, tile.pos, tile.id
                         player.isBlockByWall = True
 
         if not player.isBlockByWall and player.state == Constant.MOVE_STATE:
